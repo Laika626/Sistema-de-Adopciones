3a46982333b04c05e0d085355e9df55deaa90e45
Laika626 <cocolylo@Fernandas-MacBook-Air-8.local>
2025-12-04 05:37:00 -0600

    Modificar sintaxis del archivo CentroAdopcion y Adopcion

--- CentroAdopcion.h
+++ CentroAdopcion.h
@@ -1,3 +1,10 @@
+/*
+ * Clase EstudioAdopcion contiene los atributos y métodos genéricos para el Estudio de adopcion.
+ */
+#ifndef ESTUDIOADOPCION_H_
+#define ESTUDIOADOPCION_H_
+#include <iostream>
+#include <string>
 #include <iostream>
 #include <string>
 
@@ -11,74 +18,106 @@ class EstudioAdopcion {
 		 std::string actividadFisica;
 
 	public:
-		EstudioAdopcion(){
-			std::string auxFecha;
-			std::cout << "Fecha: ";
-			std::cin >> auxFecha;
-			fecha = auxFecha;
-
-			std::string auxLugar;
-			std::cout << "Lugar de la adopcion: ";
-			std::cin >> auxLugar;
-			lugar = auxLugar;
-
-			int auxHijos;
-			std::cout << "Cantidad de hijos de los adoptantes: ";
-			std::cin >> auxHijos;
-			hijos = auxHijos;
-
-			int auxMascotasExistentes;
-			std::cout << "Cantidad de mascotas que tienen los adoptantes: ";
-			std::cin >> auxMascotasExistentes;
-			mascotasExistentes = auxMascotasExistentes;
-
-			int auxTiempoParaMascotas;
-			std::cout << "Cantidad de tiempo que se le dedicara a la mascota: ";
-			std::cin >> auxTiempoParaMascotas;
-			tiempoParaMascota = auxTiempoParaMascotas;
-
-			std::string auxActividadFisica;
-			std::cout << "Nivel de actividad fisica de la persona (B, M, A): ";
-			std::cin >> auxActividadFisica;
-			actividadFisica = auxActividadFisica;
-		}
-
-		std::string getFecha() {
-			return fecha;
-		} 
-		std::string getLugar() {
-			return lugar;
-		} 
-		int getHijos () {
-			return hijos;
-		}
-		int getMascotasExistentes() {
-			return mascotasExistentes;
-		}
-		int getTiempoParaMascota() {
-			return tiempoParaMascota;
-		}
-		std::string getActividadFisica() {
-			return actividadFisica;
-		} 
-
-		void setFecha(std::string newFecha) {
-			fecha = newFecha;
-		} 
-		void setLugar(std::string newLugar) {
-			lugar = newLugar;
-		} 
-		void setHijos(int newHijos) {
-			hijos = newHijos;
-		} 
-		void setMascotasExistentes(int newMascotasExistentes) {
-			mascotasExistentes = newMascotasExistentes;
-		} 
-		void setTiempoParaMascotas(int newTiempoParaMascotas) {
-			tiempoParaMascota = newTiempoParaMascotas;
-		} 
-		void setActividadFisica(std::string newActividadFisica) {
-			actividadFisica = newActividadFisica;
-		} 
+		EstudioAdopcion();
+		std::string getFecha();
+		std::string getLugar();
+		int getHijos ();
+		int getMascotasExistentes();
+		int getTiempoParaMascota();
+		std::string getActividadFisica();
+		void setFecha(std::string newFecha);
+		void setLugar(std::string newLugar);
+		void setHijos(int newHijos);
+		void setMascotasExistentes(int newMascotasExistentes);
+		void setTiempoParaMascotas(int newTiempoParaMascotas);
+		void setActividadFisica(std::string newActividadFisica);
 
 };
+
+EstudioAdopcion::EstudioAdopcion(){
+	std::string auxFecha;
+	std::cout << "Fecha: ";
+	std::cin >> auxFecha;
+	fecha = auxFecha;
+
+	std::string auxLugar;
+	std::cout << "Lugar de la adopcion: ";
+	std::cin >> auxLugar;
+	lugar = auxLugar;
+
+	int auxHijos;
+	std::cout << "Cantidad de hijos de los adoptantes: ";
+	std::cin >> auxHijos;
+	hijos = auxHijos;
+
+	int auxMascotasExistentes;
+	std::cout << "Cantidad de mascotas que tienen los adoptantes: ";
+	std::cin >> auxMascotasExistentes;
+	mascotasExistentes = auxMascotasExistentes;
+
+	int auxTiempoParaMascotas;
+	std::cout << "Cantidad de tiempo que se le dedicara a la mascota: ";
+	std::cin >> auxTiempoParaMascotas;
+	tiempoParaMascota = auxTiempoParaMascotas;
+
+	std::string auxActividadFisica;
+	std::cout << "Nivel de actividad fisica de la persona (B, M, A): ";
+	std::cin >> auxActividadFisica;
+	actividadFisica = auxActividadFisica;
+}
+
+std::string EstudioAdopcion::getFecha() {
+	return fecha;
+} 
+
+std::string EstudioAdopcion::getLugar() {
+	return lugar;
+}
+
+int EstudioAdopcion::getHijos () {
+	return hijos;
+}
+
+int EstudioAdopcion::getMascotasExistentes() {
+	return mascotasExistentes;
+}
+		
+int EstudioAdopcion::getTiempoParaMascota() {
+	return tiempoParaMascota;
+}
+		
+std::string EstudioAdopcion::getActividadFisica() {
+	return actividadFisica;
+} 
+
+void EstudioAdopcion::setFecha(std::string newFecha) {
+	fecha = newFecha;
+} 
+
+void EstudioAdopcion::setLugar(std::string newLugar) {
+	lugar = newLugar;
+} 
+		
+void EstudioAdopcion::setHijos(int newHijos) {
+	hijos = newHijos;
+} 
+
+void EstudioAdopcion::setMascotasExistentes(int newMascotasExistentes) {
+	mascotasExistentes = newMascotasExistentes;
+} 
+		
+void EstudioAdopcion::setTiempoParaMascotas(int newTiempoParaMascotas) {
+	tiempoParaMascota = newTiempoParaMascotas;
+} 
+		
+void EstudioAdopcion::setActividadFisica(std::string newActividadFisica) {
+	actividadFisica = newActividadFisica;
+} 
+
+#endif
+
+
+
+
+
+
--- adopcion.h
+++ adopcion.h
@@ -1,3 +1,8 @@
+/*
+ * Clase Adopcion contiene los atributos y métodos necesarios.
+ */
+#ifndef ADOPCION_H_
+#define ADOPCION_H_
 #include <iostream>
 #include <string>
 #include "Persona.h"
@@ -16,40 +21,55 @@ class Adopcion {
 	EstudioAdopcion estudioAdopcion;
 
 	public:
-	Adopcion(Adoptante *auxAdoptante, Mascota *auxMascota, Rescatista *auxRescatista){
-		adoptante = auxAdoptante;
-		mascota = auxMascota;
-		rescatista = auxRescatista;
-
-
-		std::string auxStatus;
-		std::cout << "Status(aprobada, rechasada, en proceso): ";
-		std::cin >> auxStatus;
-		status = auxStatus;
-	}
-
-	std::string getStatus() {
-			return status;
-		} 
-
-	void setStatus(std::string newStatus) {
-			status = newStatus;
-		} 
-
-
-	void imprimirAdopcionLight(){
-		std::cout << "Mascota: " << mascota->getNombre() << std::endl;
-		std::cout << "Adoptante: " << adoptante->getNombre() << std::endl;
-		std::cout << "Rescatista: " << rescatista->getNombre() << std::endl;
-		std::cout << "Status: " << status << std::endl;
-	}
-
-	void modificacionStatusAdopcion(){
-		std::cout << "Nuevo Status de adopcion: ";
-        std::string nuevoStatus;
-        std::cin >> nuevoStatus;
-        setStatus(nuevoStatus);
-    	
-	}
+		Adopcion(Adoptante *auxAdoptante, Mascota *auxMascota, Rescatista *auxRescatista);
+		std::string getStatus();
+		void setStatus(std::string newStatus);
+		void imprimirAdopcionLight();
+		void modificacionStatusAdopcion();
 	
 };
+
+Adopcion::Adopcion(Adoptante *auxAdoptante, Mascota *auxMascota, Rescatista *auxRescatista){
+	adoptante = auxAdoptante;
+	mascota = auxMascota;
+	rescatista = auxRescatista;
+
+
+	std::string auxStatus;
+	std::cout << "Status(aprobada, rechasada, en proceso): ";
+	std::cin >> auxStatus;
+	status = auxStatus;
+}
+
+
+std::string Adopcion::getStatus() {
+	return status;
+} 
+
+
+void Adopcion::setStatus(std::string newStatus) {
+	status = newStatus;
+} 
+
+
+void Adopcion::imprimirAdopcionLight(){
+	std::cout << "Mascota: " << mascota->getNombre() << std::endl;
+	std::cout << "Adoptante: " << adoptante->getNombre() << std::endl;
+	std::cout << "Rescatista: " << rescatista->getNombre() << std::endl;
+	std::cout << "Status: " << status << std::endl;
+}
+
+
+void Adopcion::modificacionStatusAdopcion(){
+	std::cout << "Nuevo Status de adopcion: ";
+    std::string nuevoStatus;
+    std::cin >> nuevoStatus;
+    setStatus(nuevoStatus);
+}
+
+
+
+#endif
+
+
+
